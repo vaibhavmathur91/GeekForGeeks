868c62173cb99964d310f3db342644370411a742
vaibhav <vaibhav09121991@gmail.com>
2019-12-31 12:17:30 +0530

    GeekforGeeks : Array : 1-4

---
+++ Arrays/5_leaders-in-an-array.py
@@ -0,0 +1,20 @@
+"""
+Leaders in an array
+Write a program to print all the LEADERS in the array.
+An element is leader if it is greater than all the elements to its right side.
+And the rightmost element is always a leader. For example int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2.
+Let the input array be arr[] and size of the array be size.
+
+"""
+
+
+def print_leaders(arr, n):
+    max_left = float("-inf")
+    for i in range(n-1, -1, -1):
+        if arr[i] > max_left:
+            print(arr[i], end=" ")
+            max_left = arr[i]
+
+
+array = [16, 17, 4, 3, 5, 2]
+print_leaders(array, len(array))
